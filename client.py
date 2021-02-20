25b21dc60dccffef915206db3b6b019b633d2234
haelles <haelle@buaa.edu.cn>
2021-02-20 15:26:11 +0800

    debug

--- client.py
+++ client.py
@@ -5,8 +5,8 @@ root = "./api/"
 url = "http://127.0.0.1:5000/generate"
 files = {'original': open(root + 'original.jpg', 'rb'),
          'sketch': open(root + 'sketch.jpg', 'rb'),
-         'mask': open(root + 'mask.jpg', 'rb'),
-         'stroke': open(root + 'stroke.jpg', 'rb')}
+         'mask': open(root + 'mask.png', 'rb'),
+         'stroke': open(root + 'stroke.png', 'rb')}
 start_time = time.time()
 resp = requests.post(url, files=files)
 print(resp)
@@ -14,3 +14,4 @@ print(resp.json())
 end_time = time.time()
 
 print("cost time {}s".format(end_time-start_time))
+
